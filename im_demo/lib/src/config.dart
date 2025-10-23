81d9274ff142db65c5322f2eb35aaa9c792a15b6
houchengdong <houchengdong@corp.netease.com>
2025-10-23 11:25:45 +0800

    删除信息

--- im_demo/lib/src/config.dart
+++ im_demo/lib/src/config.dart
@@ -13,16 +13,16 @@ import 'package:nim_chatkit/im_kit_client.dart';
 
 class IMDemoConfig {
   //云信IM appKey
-  static const AppKey = '3e215d27b6a6a9e27dad7ef36dd5b65c';
+  static const AppKey = 'your app key';
 
   //高德Android Key
-  static const AMapAndroid = 'ff1b6763d4cc688d9cc3670ad5363a3a';
+  static const AMapAndroid = 'your amap android key';
 
   //高德IOS Key
-  static const AMapIOS = '42a4c444bb7090955b6dd03e20848710';
+  static const AMapIOS = 'your amap ios key';
 
   //高德Web服务端 Key，用于生成静态图
-  static const AMapWeb = '378d41cccf6b1253672ff69393ad70da';
+  static const AMapWeb = 'your amap web key';
 }
 
 class NIMSDKOptionsConfig {
@@ -69,6 +69,7 @@ class NIMSDKOptionsConfig {
   }
 
   static Future<NIMStatusBarNotificationConfig>
+  //todo 设置Android通知栏点击跳转类
       loadStatusBarNotificationConfig() async {
     final config = await ConfigRepo.getStatusBarNotificationConfig();
     if (config == null) {
@@ -85,50 +86,28 @@ class NIMSDKOptionsConfig {
   }
 
   static NIMMixPushConfig? _buildMixPushConfig() {
+    //todo Android推送配置，请这是自己的信息
     return NIMMixPushConfig(
       // xiaomi
-      xmAppId: '2882303761520055541',
-      xmAppKey: '5222005592541',
-      xmCertificateName: 'KIT_FLUTTER_MI_PUSH',
+      xmAppId: 'xmAppId',
+      xmAppKey: 'xmAppKey',
+      xmCertificateName: 'xmCertificateName',
       // huawei
-      hwAppId: '106776305',
-      hwCertificateName: 'KIT_FLUTTER_HW_PUSH',
+      hwAppId: 'hwAppId',
+      hwCertificateName: 'hwCertificateName',
       // meizu
-      mzAppId: '149497',
-      mzAppKey: '59aea173afc94791ad271f7d51e4bded',
-      mzCertificateName: 'KIT_FLUTTER_MEIZU_PUSH',
+      mzAppId: 'mzAppId',
+      mzAppKey: 'mzAppKey',
+      mzCertificateName: 'mzCertificateName',
       // fcm
       // fcmCertificateName: 'DEMO_FCM_PUSH',
       // vivo
-      vivoCertificateName: 'KIT_FLUTTER_VIVO_PUSH',
+      vivoCertificateName: 'vivoCertificateName',
       // oppo
-      oppoAppId: '30853511',
-      oppoAppKey: 'b2fe114b4f744f0ca6855731d18a2d54',
-      oppoAppSecret: 'dc093c8c4d154722a75cc3a69af73ce9',
-      oppoCertificateName: 'KIT_FLUTTER_OPPO_PUSH',
+      oppoAppId: 'oppoAppId',
+      oppoAppKey: 'oppoAppKey',
+      oppoAppSecret: 'oppoAppSecret',
+      oppoCertificateName: 'oppoCertificateName',
     );
   }
-}
-
-///获取当前环境, true: 测试环境，false: 正式环境
-///todo 发布前删除，此方法不对外
-Future<bool> isDebugModel() async {
-  bool isDebug = false;
-  try {
-    var value = await rootBundle.loadString('assets/config.properties');
-    var list = value.split('\n');
-    Alog.i(tag: 'isDebugModel', content: 'list: $list');
-    for (var element in list) {
-      if (element.contains('=')) {
-        var key = element.substring(0, element.indexOf('='));
-        var value = element.substring(element.indexOf('=') + 1);
-        if (key == 'ENV') {
-          isDebug = value != 'ONLINE';
-        }
-      }
-    }
-  } catch (e) {
-    Alog.e(tag: 'isDebugModel', content: e.toString());
-  }
-  return isDebug;
 }
